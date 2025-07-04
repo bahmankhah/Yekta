a2732702749fc91e0ab5f1d65603d45e2fe49ac4
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2025-07-04 17:03:03 +0330

    Rename options and expand SSO guards

--- src/Providers/PanelServiceProvider.php
+++ src/Providers/PanelServiceProvider.php
@@ -22,16 +22,29 @@ class PanelServiceProvider
                 );
         });
         Wordpress::action('admin_init',function(){
-            register_setting('my_sso_options_group', 'my_sso_method', ['sanitize_callback' => 'sanitize_text_field']);
-            register_setting('my_sso_options_group', 'my_sso_client_id', ['sanitize_callback' => 'sanitize_text_field']);
-            register_setting('my_sso_options_group', 'my_sso_login_url', ['sanitize_callback' => 'esc_url_raw']);
-            register_setting('my_sso_options_group', 'my_sso_secret_key', ['sanitize_callback' => 'sanitize_text_field']);
-            register_setting('my_sso_options_group', 'my_sso_token_endpoint', ['sanitize_callback' => 'esc_url_raw']);
-            register_setting('my_sso_options_group', 'my_sso_redirect_url', ['sanitize_callback' => 'esc_url_raw']);
-            register_setting('my_sso_options_group', 'my_sso_validate_url', ['sanitize_callback' => 'esc_url_raw']);
-            register_setting('my_sso_options_group', 'my_sso_logout_url', ['sanitize_callback' => 'esc_url_raw']);
-            register_setting('my_sso_options_group', 'my_sso_public_key');
-            register_setting('my_sso_options_group', 'my_sso_userinfo_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_method', ['sanitize_callback' => 'sanitize_text_field']);
+
+            register_setting('yekta_sso_options_group', 'yekta_sso_token_guard_client_id', ['sanitize_callback' => 'sanitize_text_field']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_token_guard_login_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_token_guard_validate_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_token_guard_redirect_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_token_guard_logout_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_token_guard_public_key');
+            register_setting('yekta_sso_options_group', 'yekta_sso_token_guard_userinfo_url', ['sanitize_callback' => 'esc_url_raw']);
+
+            register_setting('yekta_sso_options_group', 'yekta_sso_password_guard_token_endpoint', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_password_guard_secret_key', ['sanitize_callback' => 'sanitize_text_field']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_password_guard_redirect_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_password_guard_logout_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_password_guard_public_key');
+            register_setting('yekta_sso_options_group', 'yekta_sso_password_guard_userinfo_url', ['sanitize_callback' => 'esc_url_raw']);
+
+            register_setting('yekta_sso_options_group', 'yekta_sso_secret_guard_token_endpoint', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_secret_guard_secret_key', ['sanitize_callback' => 'sanitize_text_field']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_secret_guard_redirect_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_secret_guard_logout_url', ['sanitize_callback' => 'esc_url_raw']);
+            register_setting('yekta_sso_options_group', 'yekta_sso_secret_guard_public_key');
+            register_setting('yekta_sso_options_group', 'yekta_sso_secret_guard_userinfo_url', ['sanitize_callback' => 'esc_url_raw']);
         });
     }
 }
