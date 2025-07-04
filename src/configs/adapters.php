4c573974ec24912387f947880184a5161adb57ea
bahmankhah <47614811+bahmankhah@users.noreply.github.com>
2025-07-04 15:57:37 +0330

    Add password and secret key SSO guards

--- src/configs/adapters.php
+++ src/configs/adapters.php
@@ -10,6 +10,12 @@ return [
                 'login_url' => 'https://tauth.platform.donap.ir/realms/donap/protocol/openid-connect/auth?client_id={clientId}&response_type=code',
                 'client_id' => 'market',
                 'validate_url'=>'https://tauth.platform.donap.ir/realms/donap/protocol/openid-connect/token'
+            ],
+            'password' => [
+                'context' => Kernel\Auth\Guards\PasswordSSOGuard::class,
+            ],
+            'secret' => [
+                'context' => Kernel\Auth\Guards\SecretKeySSOGuard::class,
             ]
         ]
     ],
